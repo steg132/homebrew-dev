d4e62747833ba2429c404e825b23a2fe5479fc90
Ryan Schumacher <Ryan.Schumacher@possible.com>
2016-11-22 19:24:39 -0600

    Adding formula for good_morning

---
+++ Formula/good_morning.rb
@@ -0,0 +1,8 @@
+class GoodMorning < Formula
+  desc ""
+  homepage ""
+  url "https://docs.google.com/uc?id=0ByZpmWOhfGPTLUliMk5nWXNWcnM&export=download"
+  sha256 "a2e9625a9cde03a4bc30956ff704eb29ebaf6c6ee85d75bca5421ab4dda639c2"
+          
+  # depends_on "cmake" => :build
+end
