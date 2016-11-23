b414e008e63baa13cacfcf851b3c7afceaa19123
Ryan Schumacher <Ryan.Schumacher@possible.com>
2016-11-22 21:08:58 -0600

    Adding good_morning formula

---
+++ Formula/good_morning.rb
@@ -0,0 +1,10 @@
+class GoodMorning < Formula
+	desc "Simple tool to updated my brew computer"
+	homepage "https://github.com/steg132"
+	head "https://github.com/steg132/good-morning.git"
+
+	def install
+		system "xcodebuild -target 'Good Morning' -scheme 'Good Morning' -configuration Deploy CONFIGURATION_BUILD_DIR=./build/"
+		bin.install "build/good_morning"
+	end
+end
