c34f42e0908c56e9eb728dd69740b83ee448c30a
Ryan Schumacher <Ryan.Schumacher@possible.com>
2016-11-22 21:30:09 -0600

    Adding version

--- Formula/good_morning.rb
+++ Formula/good_morning.rb
@@ -2,6 +2,7 @@ class GoodMorning < Formula
 	desc "Simple tool to updated my brew computer"
 	homepage "https://github.com/steg132"
 	url "https://github.com/steg132/good-morning.git"
+	version "1.0.0"
 
 	def install
 		system "xcodebuild -target 'Good Morning' -configuration Deploy CONFIGURATION_BUILD_DIR=./build/"
